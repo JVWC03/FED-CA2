b760028644160f6d9d5ebf4fd468a421ca589c6a
Jack Coughlan <jackvruss@gmail.com>
2025-12-01 13:11:25 +0000

    Create Doctor and view doctor list functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,32 @@ import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 import { AppSidebar } from '@/components/app-sidebar';
 import { SiteHeader } from '@/components/site-header';
 
+import Navbar from '@/components/Navbar';
 import Home from '@/pages/Home';
+
+// Doctor pages
+import DoctorsIndex from '@/pages/doctors/Index';
+// import DoctorsShow from '@/pages/doctors/Show';
+import DoctorsCreate from '@/pages/doctors/Create';
+// import DoctorsEdit from '@/pages/doctors/Edit';
 
 export default function App() {
   const [loggedIn, setLoggedIn] = useState(false);
 
   useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token) setLoggedIn(true);
+    let token = localStorage.getItem("token");
+    if(token){
+      setLoggedIn(true);
+    }
   }, []);
 
   const onLogin = (auth, token) => {
     setLoggedIn(auth);
-    if (auth) {
-      localStorage.setItem('token', token);
-    } else {
+
+    if(auth){
+      localStorage.setItem('token', token)
+    }
+    else {
       localStorage.removeItem('token');
     }
   };
@@ -36,13 +47,24 @@ export default function App() {
         <SidebarInset>
           <SiteHeader />
 
-          <div className="flex flex-1 flex-col p-6">
-            <Routes>
-              <Route
-                path="/"
-                element={<Home onLogin={onLogin} loggedIn={loggedIn} />}
-              />
-            </Routes>
+          <div className="flex flex-1 flex-col">
+            <div className="@container/main flex flex-1 flex-col gap-2">
+              <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6 mx-6">
+                {/* Main content */}
+                <Routes>
+                  <Route
+                    path="/"
+                    element={<Home onLogin={onLogin} loggedIn={loggedIn} />}
+                  />
+
+                  {/* Doctor routes */}
+                  <Route path="/doctors" element={<DoctorsIndex />} />
+                  {/* <Route path="/doctors/:id" element={<DoctorsShow loggedIn={loggedIn} />} /> */}
+                  <Route path="/doctors/create" element={<DoctorsCreate />} />
+                  {/* <Route path="/doctors/:id/edit" element={<DoctorsEdit />} /> */}
+                </Routes>
+              </div>
+            </div>
           </div>
         </SidebarInset>
       </SidebarProvider>
---
+++ src/pages/doctors/index.jsx
@@ -0,0 +1,77 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router';
+import { Button } from '@/components/ui/button';
+
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+export default function Index() {
+  const [doctors, setDoctors] = useState([]);
+  const token = localStorage.getItem("token");
+
+  useEffect(() => {
+    const fetchDoctors = async () => {
+      const options = {
+        method: "GET",
+        url: "https://ca2-med-api.vercel.app/doctors",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      try {
+        const response = await axios.request(options);
+        console.log(response.data);
+        setDoctors(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchDoctors();
+  }, []);
+
+  return (
+    <>
+      <Button
+        asChild
+        variant="primary"
+        className="mb-4 mr-auto-block"
+      >
+        <Link size="sm" to={`/doctors/create`}>Create New Doctor</Link>
+      </Button>
+
+      <Table>
+        <TableCaption>List of registered doctors.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>First Name</TableHead>
+            <TableHead>Last Name</TableHead>
+            <TableHead>Email</TableHead>
+            <TableHead>Phone</TableHead>
+            <TableHead>Specialisation</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {doctors.map((doctor) => (
+            <TableRow key={doctor.id}>
+              <TableCell>{doctor.first_name}</TableCell>
+              <TableCell>{doctor.last_name}</TableCell>
+              <TableCell>{doctor.email}</TableCell>
+              <TableCell>{doctor.phone}</TableCell>
+              <TableCell>{doctor.specialisation}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </>
+  );
+}
